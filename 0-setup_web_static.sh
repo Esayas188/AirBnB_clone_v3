8d7972a8cc9b2d4bd472a2e0ba378d3476b91a23
Esayas188 <110990267+Esayas188@users.noreply.github.com>
2023-07-06 04:24:01 -0400

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,23 @@
 #!/usr/bin/env bash
+# Sets up a web server for deployment of web_static.
 
-# Install Nginx if not already installed
-if [ $(dpkg-query -W -f='${Status}' nginx 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-    sudo apt-get update
-    sudo apt-get install -y nginx
-fi
+apt-get update
+apt-get install -y nginx
 
-# Create necessary directories
-sudo mkdir -p /data/web_static/releases /data/web_static/shared /data/web_static/releases/test
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create fake HTML file for testing
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Create symbolic link
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-
-# Set ownership of /data/ to ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-printf '%s\n' 'server {
+printf %s "server {
     listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By $HOSTNAME;
-    root /var/www/html;
-    index index.html index.htm;
+    root   /var/www/html;
+    index  index.html index.htm;
 
     location /hbnb_static {
         alias /data/web_static/current;
@@ -37,10 +30,9 @@ printf '%s\n' 'server {
 
     error_page 404 /404.html;
     location /404 {
-        root /var/www/html;
-        internal;
+      root /var/www/html;
+      internal;
     }
-}' | sudo tee /etc/nginx/sites-available/default > /dev/null
+}" > /etc/nginx/sites-available/default
 
-# Restart Nginx
-sudo service nginx restart
+service nginx restart
